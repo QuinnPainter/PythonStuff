4a8e3e415943d85bdfadf9ede2e40d931100f16f
Quinn Painter <quinn@geekymonkey.com>
2017-11-07 20:25:23 +0000

    Start of recursive tile revealing

--- AsciiSweeper/asciisweeper.py
+++ AsciiSweeper/asciisweeper.py
@@ -1,7 +1,6 @@
 import random
 #import string
 import sys
-import colorama
 
 size = 9
 diff = 25
@@ -10,14 +9,11 @@ mineboard = []
 groundchar = "O"
 minechar = "M"
 flagchar = "F"
-groundcolour = colorama.Fore.GREEN# + colorama.Back.GREEN
-minecolour = colorama.Fore.YELLOW
-flagcolour = colorama.Fore.RED
 pretty = True
 
 def genBoard():
     for y in xrange(size):
-        mineboard.append(groundchar * size)
+        mineboard.append(" " * size)
         seenboard.append(groundchar * size)
     availableMinePositions = []
     for y in xrange(size):
@@ -31,6 +27,76 @@ def genBoard():
         del availableMinePositions[mineposInList]
         #minepos = [random.randint(0, size - 1), random.randint(0, size - 1)]
         mineboard[minepos[0]] = mineboard[minepos[0]][:minepos[1]] + minechar + mineboard[minepos[0]][minepos[1] + 1:]
+    for position in availableMinePositions:
+        #Loop through every coordinate that is not a bomb, and assign it a number
+        #If it is not a number or a bomb, it will remain a blank space
+        position = [int(position[0]), int(position[1])]
+        numberValue = 0
+        tilesToCheck = ["TL", "T", "TR", "L", "R", "BL", "B", "BR"]# Protective measures to prevent array index errors and list wrap
+        if position[1] == 0:
+            #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "L"+ mineboard[position[0]][position[1] + 1:]
+            #left edge
+            tilesToCheck.remove("TL")
+            tilesToCheck.remove("L")
+            tilesToCheck.remove("BL")
+        if position[1] == size - 1:
+            #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "R"+ mineboard[position[0]][position[1] + 1:]
+            #right edge
+            tilesToCheck.remove("TR")
+            tilesToCheck.remove("R")
+            tilesToCheck.remove("BR")
+        if position[0] == 0:
+            #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "T"+ mineboard[position[0]][position[1] + 1:]
+            #top edge
+            try:
+                tilesToCheck.remove("TL")
+            except ValueError:
+                pass #already removed
+            tilesToCheck.remove("T")
+            try:
+                tilesToCheck.remove("TR")
+            except ValueError:
+                pass #already removed
+        if position[0] == size - 1:
+            #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "B"+ mineboard[position[0]][position[1] + 1:]
+            #bottom edge
+            try:
+                tilesToCheck.remove("BL")
+            except ValueError:
+                pass #already removed
+            tilesToCheck.remove("B")
+            try:
+                tilesToCheck.remove("BR")
+            except ValueError:
+                pass #already removed
+        for tile in tilesToCheck:
+            relativeCoordinates = [0, 0]
+            if tile == "TL":
+                relativeCoordinates = [-1, -1]
+            elif tile == "T":
+                relativeCoordinates = [0, -1]
+            elif tile == "TR":
+                relativeCoordinates = [1, -1]
+            elif tile == "L":
+                relativeCoordinates = [-1, 0]
+            elif tile == "R":
+                relativeCoordinates = [1, 0]
+            elif tile == "BL":
+                relativeCoordinates = [-1, 1]
+            elif tile == "B":
+                relativeCoordinates = [0, 1]
+            elif tile == "BR":
+                relativeCoordinates = [1, 1]
+            checkPosition = [position[0] + relativeCoordinates[1], position[1] + relativeCoordinates[0]]
+            if mineboard[checkPosition[0]][checkPosition[1]] == minechar:
+                numberValue += 1
+        #if (mineboard[position[1]][position[0] - 1] == minechar): #directly to left
+        #    numberValue += 1
+        #if (mineboard[position[1]][position[0] + 1] == minechar): #directly to right
+        #    numberValue += 1
+        
+        if numberValue > 0:
+            mineboard[position[0]] = mineboard[position[0]][:position[1]] + str(numberValue)+ mineboard[position[0]][position[1] + 1:]
 def showBoard():
     if (pretty == True):
         numbers = range(1, size + 1)
@@ -59,23 +125,100 @@ def showBoard():
         print "   " + "".join(numberrow2)
     else:
         for r in xrange(size):
+            print mineboard[r]
+        for r in xrange(size):
             print seenboard[r]
 def gameLoop():
     global seenboard
     action = getInput()
     if action[0] == "flag":
-        l = list(seenboard[size - action[2]]) #Convert selected row to list so we can assign a single character, also reverse it so input goes from bottom left instead of top left
-        l[action[1] - 1] = flagchar
-        seenboard[size - action[2]] = "".join(l) #...and convert it back to string
+        setCharAtPosition(action[1], action[2], flagchar)
         showBoard()
     elif action[0] == "dig":
-        print mineboard[size - action[2]][action[1]]
-        if mineboard[size - action[2]][action[1]] == minechar:
+        #print mineboard[size - action[2]][action[1]]
+        if mineboard[size - action[2]][action[1] - 1] == minechar:
             lose(action[1], action[2])
+        reveal(action[1], action[2])
+        showBoard()
     gameLoop()
 def lose(bombx, bomby):
     print "You lose" #todo
     sys.exit()
+def setCharAtPosition(x, y, char):
+    l = list(seenboard[size - y]) #Convert selected row to list so we can assign a single character, also reverse it so input goes from bottom left instead of top left
+    l[x - 1] = char
+    seenboard[size - y] = "".join(l) #...and convert it back to string
+def reveal(x, y):
+    revealOneTile(x, y)
+    if mineboard[size - y][x - 1] == " ":
+        nearTiles = NearbyTileCoords(x, y)
+        for tile in nearTiles:
+            reveal(tile[1], tile[0])
+    else: #Must be a number
+        pass #No need to do anything here yet
+def revealOneTile(x, y):
+    setCharAtPosition(x, y, mineboard[size - y][x - 1])
+def NearbyTileCoords(x, y):
+    relativeCoords = []
+    worldCoords = []
+    tilesToCheck = ["TL", "T", "TR", "L", "R", "BL", "B", "BR"]# Protective measures to prevent array index errors and list wrap
+    if x-1 == 0:
+        #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "L"+ mineboard[position[0]][position[1] + 1:]
+        #left edge
+        tilesToCheck.remove("TL")
+        tilesToCheck.remove("L")
+        tilesToCheck.remove("BL")
+    if x-1 == size - 1:
+        #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "R"+ mineboard[position[0]][position[1] + 1:]
+        #right edge
+        tilesToCheck.remove("TR")
+        tilesToCheck.remove("R")
+        tilesToCheck.remove("BR")
+    if y-1 == 0:
+        #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "T"+ mineboard[position[0]][position[1] + 1:]
+        #top edge
+        try:
+            tilesToCheck.remove("TL")
+        except ValueError:
+            pass #already removed
+        tilesToCheck.remove("T")
+        try:
+            tilesToCheck.remove("TR")
+        except ValueError:
+            pass #already removed
+    if y-1 == size - 1:
+        #mineboard[position[0]] = mineboard[position[0]][:position[1]] + "B"+ mineboard[position[0]][position[1] + 1:]
+        #bottom edge
+        try:
+            tilesToCheck.remove("BL")
+        except ValueError:
+            pass #already removed
+        tilesToCheck.remove("B")
+        try:
+            tilesToCheck.remove("BR")
+        except ValueError:
+            pass #already removed
+    for tile in tilesToCheck:
+        if tile == "TL":
+            relativeCoords.append([-1, -1])
+        elif tile == "T":
+            relativeCoords.append([0, -1])
+        elif tile == "TR":
+            relativeCoords.append([1, -1])
+        elif tile == "L":
+            relativeCoords.append([-1, 0])
+        elif tile == "R":
+            relativeCoords.append([1, 0])
+        elif tile == "BL":
+            relativeCoords.append([-1, 1])
+        elif tile == "B":
+            relativeCoords.append([0, 1])
+        elif tile == "BR":
+            relativeCoords.append([1, 1])
+    for c in relativeCoords:
+        worldCoords.append([y + c[1], x + c[0]])
+    print worldCoords
+    return worldCoords
 def getInput():
     input = raw_input().lower() #case insensitive
     command = input[:4] #the command is always the first 4 letters: help, exit, flag
@@ -117,13 +260,25 @@ def cleanCoordinates(s):
     if (x > size or y > size or x < 1 or y < 1):
         return [-998, -998]
     return [x, y]
-if (len(sys.argv) != 3):
-    print ("Incorrect arguments")
+if (len(sys.argv) != 4 or sys.argv[1] == "help"):
+    #print ("Incorrect arguments")
     print ("First arg is size, second is difficulty (number of mines)")
+    print ("Third argument is to present with colour and numbers, True or False")
     exit()
 size = int(sys.argv[1])
 diff = int(sys.argv[2])
-colorama.init(autoreset=True) #Init script required for Colorama
+pretty = sys.argv[3]
+if pretty == "True":
+    import colorama
+    colorama.init(autoreset=True) #Init script required for Colorama
+    groundcolour = colorama.Fore.GREEN# + colorama.Back.GREEN
+    minecolour = colorama.Fore.YELLOW
+    flagcolour = colorama.Fore.RED
+    pretty = True
+elif pretty == "False":
+    pretty = False
+else:
+    print ("Third argument must be True or False")
 genBoard()
 showBoard()
 print "Generated new board. Type 'help' for help."
